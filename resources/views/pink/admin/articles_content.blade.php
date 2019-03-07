d4639153359842100e5febea3fd7160b301ea3fd
smbat <smbatpaloyan@gmail.com>
2019-03-07 20:19:55 +0400

    change my request in my CommentController to CommentReque

--- resources/views/pink/admin/articles_content.blade.php
+++ resources/views/pink/admin/articles_content.blade.php
@@ -1,5 +1,5 @@
 @if($articles)
-    <div id="content-page" class="content-group">
+    <div id="content-page" class="content group">
         <div class="hentry group">
             <h2>Articles</h2>
             <div class="short-table white">
@@ -19,13 +19,15 @@
                         @foreach($articles as $article)
                             <tr>
                                 <td class="align-left">{{ $article->id }}</td>
-                                <td class="align-left"><a href="{{route('articles.edit',$article->alias)}}">{{$article->title}}</a></td>
+                                <td class="align-left"><a href="{{route('adminArticles.edit',$article->alias)}}">{{$article->title}}</a></td>
                                 <td class="align-left">{{ str_limit($article->text,200) }}</td>
                                 <td><img src="{{ asset(env('THEME')) }}/images/articles/{{ $article->img->mini }}"></td>
                                 <td>{{ $article->category->title }}</td>
                                 <td>{{ $article->alias }}</td>
                                 <td>
-                                    <form method="post" action="{{route('articles.destroy',$article->alias)}}" class="form-horizontal">
+                                    <form method="post" action="{{route('adminArticles.destroy',$article->alias)}}" class="form-horizontal">
+                                        {{csrf_field()}}
+                                        <input type="hidden" name="_method" value="DELETE">
                                         <button type="submit" class="btn btn-french-5">Delete</button>
                                     </form>
                                 </td>
